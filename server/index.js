4a0bb1be597321e35817cc92c0bbf8b6ab6e2fb1
Bryan Holder <bryanholder84@gmail.com>
2021-01-12 07:10:18 -0500

    db initialization script, server with post and get methods

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//ROUTES//
+
+//create a todo
+
+app.post('/todos', async(req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES($1) RETURNING *', 
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//get all todos
+
+app.get('/todos', async(req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+//get a todo
+
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * from todo WHERE todo_id = $1', [id]);
+
+    res.json(todo.rows[0]);
+  } catch(err) {
+    console.error(err.message);
+  }
+})
+
+//update a todo
+
+app.listen(5000, () => {
+  console.log('server has started on port 5000');
+})
