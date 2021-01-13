c934dc7ba71e646943b58d329fa0739032a3ed80
Bryan Holder <bryanholder84@gmail.com>
2021-01-13 06:03:09 -0500

    Implemented PUT and DELETE methods

--- server/index.js
+++ server/index.js
@@ -51,6 +51,34 @@ app.get('/todos/:id', async (req, res) => {
 
 //update a todo
 
+app.put('/todos/:id', async(req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
+      [description, id]
+    );
+
+    res.json('Todo was updated!');
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+
+    res.json('Todo was deleted!');
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
 app.listen(5000, () => {
   console.log('server has started on port 5000');
 })
