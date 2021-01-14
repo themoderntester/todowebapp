fa848d1898f2b9e2f57537a8c56af41dc19c02c1
Bryan Holder <bryanholder84@gmail.com>
2021-01-14 06:51:53 -0500

    Added EditTodo and ListTodos components

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,8 +1,25 @@
 import React,{ Fragment, useEffect, useState } from 'react';
 
+import EditTodo from './EditTodo';
+
 const ListTodos = () => {
 
   const [todos, setTodos] = useState([]);
+
+  //delete todo function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE'
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+
+    } catch(err) {
+      console.error(err.message);
+    }
+  };
 
   const getTodos = async() => {
     try {
@@ -32,10 +49,12 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map(todo => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <EditTodo todo={todo}/>
+              </td>
+              <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
